736aaa3adbaa2cc102799d928236b112eb21e7d6
faithie <faith@toyridge.com>
2025-02-24 08:37:02 -0600

    monday morninng work

--- main.c
+++ main.c
@@ -4,15 +4,78 @@
 #include <stdio.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+
+#define PACKET_SIZE 3
+#define DOUBLE_SIZE sizeof(double)
+volatile sig_atomic_t time_expired = 0;
+
+int checkError(int val, const char *msg) {
+    if (val == -1) {
+        perror(msg);
+        exit(EXIT_FAILURE);
+    }
+    return val;
+}
+
+void signalHandler(int sig){
+    char response;
+
+    // Sigint handles control c
+    if(sig == SIGINT){
+        printf("\nExit (y/n)? ");
+        scanf(" %c", &response);
+        if (response == 'y' || response == 'Y') {
+            exit(EXIT_SUCCESS);
+        }
+    }
+    // Handles timer expiring
+    if(sig == SIGALRM){
+        time_expired = 1;
+        printf("\nTime's UP!\n");
+    }
+}
 
 int main(){
 
 
     pid_t childPID;
+    const char *input_file = "angl.dat";
 
-    // childPID = fork();
-    // Returns a -1 on error. Returns a 0 in the child. Returns the process id for the child in the parents
+    struct sigaction sa_timer;
+    sa_timer.sa_handler = signalHandler;
+    sa_timer.sa_flags = 0;
+    sigemptyset(&sa_timer.sa_mask);
+    sigaction(SIGALRM, &sa_timer, NULL);
 
+    // Control C
+    struct sigaction sa_int;
+    sa_int.sa_handler = signalHandler;
+    sa_int.sa_flags = 0;
+    sigemptyset(&sa_int.sa_mask);
+    sigaction(SIGINT, &sa_int, NULL);
+
+    int input_fd = checkError(open(input_file, O_RDONLY), "Open angl.dat");
+
+    double buffer[PACKET_SIZE*DOUBLE_SIZE];
+
+    while (read(input_fd, buffer, DOUBLE_SIZE * PACKET_SIZE) == DOUBLE_SIZE * PACKET_SIZE)
+    {
+        // If range conditions are met for roll and pitch, print values   
+        double roll = buffer[0];
+        double pitch = buffer[1];
+        double yaw = buffer[2];
+
+
+        if(roll > -20 && roll < 20 && pitch > -20 && pitch < 20)
+        {
+            printf("Values for roll and pitch inside the range -20 to 20\n");
+            printf("Roll: %.2lf, Pitch: %.2lf \n", roll, pitch);
+            fflush(stdout);
+        }
+    }
+
+    // Creates child processes
     switch(childPID=fork())
     {
     case -1:
